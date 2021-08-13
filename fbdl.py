8edcfcca94b3d618e094f7b31cf7e843c0908e84
sgtreaz35 <77036037+sgtreaz35@users.noreply.github.com>
2021-08-14 01:41:15 +0600

    Create fbdl.py

---
+++ fbdl.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+import sys
+import os
+import re,time
+import requests as r
+import wget
+os.system('clear')
+def bannar():
+  print ('''\033[95m
+    
+  ___ ___   __   _____ ___  ___ ___  ___ 
+ | __| _ )__\ \ / /_ _|   \| __/ _ \/ __|
+ | _|| _ \___\ V / | || |) | _| (_) \__ \
+ |_| |___/    \_/ |___|___/|___\___/|___/
+                                         ''')
+  print ('''\033[94m
+    
+   _____           _     _               
+  / ____|         | |   | |              
+ | |  __ _ __ __ _| |__ | |__   ___ _ __ 
+ | | |_ | '__/ _` | '_ \| '_ \ / _ \ '__|
+ | |__| | | | (_| | |_) | |_) |  __/ |   
+  \_____|_|  \__,_|_.__/|_.__/ \___|_|   
+                                         
+                                         \033[93m Version : 1.0     
+                                    
+''')
+  print ('\033[94m-'*50)
+  print ('')
+  print (' \033[96m Author   : \033[95m RaRe BlAcK')
+  print ('')
+  print (' \033[96m Tool     : \033[95m FB Videos Grabber')
+  print ('')
+  print (' \033[96m Facebook : \033[95m https://www.facebook.com/')
+  print ('')
+  print (' \033[96m Github   : \033[95m https://github.com/sgtreaz35/')
+  print ('')
+  print (' \033[96m Coded By : \033[95m RaRe bLaCk , RaRe bLaCk')
+  print ('')
+  print ('\033[94m-'*50)
+  print ('')
+  print ('             \033[0;37;41m FB Videos Grabber \033[0m ' )
+  print ('')
+  print ('')
+bannar()
+
+filedir = os.path.join('file.mp4')
+ERASE_LINE = '\x1b[2K'
+hibabe=input("\033[92m [!] Paste Your FB Video Url : \033[93m ")
+
+print ('')
+print ('')
+print( '\033[0;37;44m Options Menu  : \033[0m ' )
+print ('')
+print ('')
+print ('\033[91m [\033[0m1\033[91m] \033[92m Download SD Format\033[90m')
+print ("")
+print ('\033[91m [\033[0m✓\033[91m] \033[92m Download HD Format\033[90m')
+print ("")
+print ("")
+choice=input(str('\033[92m [!] Type Any Number : \033[93m '))
+print ("")
+print ("")
+print ("\033[96m [✓] WOW Downloading Video Wait Dear..Its Depends On Your Network Speed.To Stop This Process CTRL + Z")
+print ("\033[91m")
+if choice=='1':
+  try:
+    LINK = hibabe
+    html = r.get(LINK)
+    sdvideo_url = re.search('sd_src:"(.+?)"', html.text)[1]
+  except r.ConnectionError as e:
+  
+    print("OOPS!! Connection Error.")
+    
+  except r.Timeout as e:
+    print("OOPS!! Timeout Error")
+  except r.RequestException as e:
+    print("OOPS!! General Error or Invalid URL")  
+  except (KeyboardInterrupt,SystemExit):
+    print("Ok ok, quitting")
+    sys.exit(1)
+  except TypeError:
+    print("Video May Private or Hd version not avilable")  
+  else:
+    sd_url = sdvideo_url.replace('sd_src:"', '')
+    print("\n")
+    print("\033[92m [+]Normal Quality:\033[93m " + sd_url)
+    print("[+] Video Started Downloading")
+    wget.download(sd_url, filedir)
+    sys.stdout.write(ERASE_LINE)
+    print("\n")
+    print("Video downloaded")
+    os.system('termux-setup-storage')
+    os.system('mv file.mp4 /sdcard')
+    print ('File Save As file.mp4 in Your Sdcard.')
+    print ('Cheack Your Sdcard')
+    print ('ALLAH HAFIZ ')
+if choice=='2':
+  try:
+    print ("\033[91m")
+    LINK = hibabe
+
+    html = r.get(LINK)
+    hdvideo_url = re.search('hd_src:"(.+?)"', html.text)[1]
+  except r.ConnectionError as e:
+  
+    print("OOPS!! Connection Error.")
+  except r.RequestException as e:
+    print("OOPS!! General Error or Invalid URL")  
+  except (KeyboardInterrupt,SystemExit):
+    print("Ok ok, quitting")
+    sys.exit(1)
+  except TypeError:
+    print("Video is Private or Hd version not avilable")    
+  except r.Timeout as e:
+    print("OOPS! Bad Luck! Timeout Error")
+  else:
+    hd_url = hdvideo_url.replace('hd_src:"', '')
+    print("\n")
+    print("\033[92m [+]Normal Quality:\033[93m " + hd_url)
+    print("[+] Video Started Downloading")
+    wget.download(hd_url, filedir)
+    sys.stdout.write(ERASE_LINE)
+    print("\n")
+    print("Video downloaded")
+    os.system('termux-setup-storage')
+    os.system('mv file.mp4 /sdcard')
+    print ('File Save As file.mp4 in Your Sdcard.')
+    print ('Cheack Your Sdcard')
+    print ('Allah Hafiz ')
+else:
+  print ('\033[91mGood Bye')
+  print ('Tnks For Support ')
+ 
+  
